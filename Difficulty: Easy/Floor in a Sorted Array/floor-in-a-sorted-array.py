e40ff0ba2ac435b670d59aac41589cf52825b4a5
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-07-10 23:55:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findFloor(self, arr, x):
+        # code here
+        
+        p1 = 0
+        p2 = len(arr) - 1
+        
+        answer = -1
+        while(p1 <=p2):
+            mid = (p1 + p2)//2
+            
+
+                
+            if arr[mid] <= x:
+                answer = mid
+                p1 = mid + 1
+                
+            elif arr[mid] > x:
+
+                p2 = mid - 1
+                
+        return answer
+                
