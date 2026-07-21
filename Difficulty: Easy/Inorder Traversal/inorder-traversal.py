3125d4151bd6c3735eae05fbbde8b50cbde8da67
Sai Raj Vadeghar <138302912+sairaj2264@users.noreply.github.com>
2026-07-21 15:27:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.py
@@ -0,0 +1,35 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def inOrder(self, root):
+        # code here
+        
+        stack = []
+        answer = []
+        
+        node = root
+        while(True):
+            
+            if node is not None:
+                stack.append(node)
+                node = node.left
+                
+            else:
+                if len(stack) == 0:
+                    break
+                node = stack.pop()
+                
+                answer.append(node.data)
+                node = node.right
+                
+        return answer
+                
+            
+            
+                
